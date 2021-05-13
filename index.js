52679d7c9456c70a0494bca312f1c73d6019435b
NicolasArayaB <nicolasaraya@gmail.com>
2021-05-13 15:17:49 +0000

    black and white style, petcloud project with repo and deploy link

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function clickHandler(e) {
     behavior: "smooth"
   });
 }
+
+const show = () => {
+  let links = document.querySelector(".petCloudLinks")
+  console.log(links.style.display)
+  if (links.style.display === "none" || links.style.display === "") {
+    links.style.display = "block";
+  } else {
+    links.style.display = "none";
+  }
+}
